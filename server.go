cc04c10bea7c21068daee7a301c30c5faff9f2f2
mgcarbonell <mcarbonell@Erebus.localdomain>
2022-01-06 20:06:03 -0800

    working server

--- server.go
+++ server.go
@@ -7,16 +7,28 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello, world!")
-	})
-
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server on port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err !=nil {
 		log.Fatal(err)
 	}
 }
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello, world!")
+}
+
 // ListenAndServe is a method exported by the http packet it allows us to start
 // the web server and specify a port to listen to.  The second argument it can
 // take is for HTTP/2 (read up on this), but for right now we can safely pass
